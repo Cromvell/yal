3f2e9bb0a793cbd3b1b384d48ad3ca0a1a82c2a1
Cromvell <nverzn@gmail.com>
2019-03-21 14:27:56 +0300

    Port to linux finished

--- yaLogger/main.c
+++ yaLogger/main.c
@@ -9,8 +9,8 @@ Logger TODO:
   + Separate src into different files
   + Print file, line and function in log
   + Increase timestamp pricesion. Capture it immediately on log call and then store
+  + Enable port to linux (and Mac OS in future(but who cares?))
 
-  - Enable port to linux (and Mac OS in future(but who cares?))
   - Add custom level creation and categories
   - Enable config files
   - Option "Max file count"
@@ -19,10 +19,12 @@ Logger TODO:
 
 #include "logger.h"
 
+#define __FILENAME__ (strrchr(__FILE__, P_PATH_SLASH) ? strrchr(__FILE__, P_PATH_SLASH) + 1 : __FILE__)
+
 //////////////////////////////////////////////////////////////////
 // External interface
 #define LOG_INIT(...) (logger__init(__VA_ARGS__))
-#define LOG(lgg, lvl, msg, ...) (print__log((lgg), (lvl), (uint16_t)__LINE__, __FILE__, __FUNCTION__, (msg), ##__VA_ARGS__))
+#define LOG(lgg, lvl, msg, ...) (print__log((lgg), (lvl), (uint16_t)__LINE__, (strrchr(__FILE__, P_PATH_SLASH) ? strrchr(__FILE__, P_PATH_SLASH) + 1 : __FILE__), __FUNCTION__, (msg), ## __VA_ARGS__))
 #define LOG_CLOSE(lgg) (logger__close(lgg))
 #define SET_LOG_LVL(lgg, lvl) (set__log__lvl(lgg, lvl))
 
@@ -30,5 +32,5 @@ Logger TODO:
 
 int main(int argc, char **argv) {
     test_main();
-    getchar();
+    //getchar();
 }
